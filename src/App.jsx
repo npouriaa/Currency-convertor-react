b8d2cf5787280d8de7f55ba8c3267fec8a05401b
PouriaNavipour <111690110+PouriaNavipour@users.noreply.github.com>
2023-09-21 12:01:06 +0330

    add animated gradient bg , style the App component

--- src/App.jsx
+++ src/App.jsx
@@ -6,8 +6,19 @@ import SelectCountry from "./components/SelectCountry";
 import SwitchCountry from "./components/SwitchCountry";
 
 const App = () => {
+  const boxStyles = {
+    marginTop : '160px',
+    background: "#fdfdfd",
+    textAlign : 'center',
+    color: '#222',
+    minHeight : '320px',
+    borderRadius : 2,
+    padding : '64px 32px',
+    boxShadow : '0px 10px 100px 79px rgba(0,0,0,0.1);',
+    position : 'relative'
+  }
   return (
-    <Container maxWidth="md" sx={{ background: "#fdfdfd" }}>
+    <Container maxWidth="md" sx={boxStyles}>
       <Typography variant="h5" sx={{ marginBottom: "32px" }}>
         Stay ahead with accurate conversions
       </Typography>
